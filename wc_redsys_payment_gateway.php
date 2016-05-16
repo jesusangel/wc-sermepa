e343dd1df5f7f88a06d5256f7254a7d6b0be7c7f
Manex Agirre <manex@egunea.eus>
2016-05-16 17:29:16 +0200

    Customize Ds_Order
    Added two filters to customize the order ID sended to TPV.

--- wc_redsys_payment_gateway.php
+++ wc_redsys_payment_gateway.php
@@ -357,6 +357,9 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 			function get_redsys_args( $order ) {
 				$order_id = $order->id;
 				$unique_order_id = str_pad( $order_id, 8, '0', STR_PAD_LEFT ) . date( 'is' );
+
+				// Customize order code for TPV
+				$unique_order_id = apply_filters("wc_myredsys_merchant_order_encode", $unique_order_id, $order_id);
 		
 				if ( 'yes' == $this->debug ) {
 					$this->log->add( 'redsys', 'Generating payment form for order #' . $order_id . '. Notify URL: ' . $this->notify_url );
@@ -629,11 +632,18 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 				        }
 				        
 				        $received_amount	= $data['Ds_Amount'];
-				        $order_id	= substr( $data['Ds_Order'], 0, 8 );
+				        //$order_id	= substr( $data['Ds_Order'], 0, 8 );
 				        $fuc		= $data['Ds_MerchantCode'];
 				        $currency	= $data['Ds_Currency'];
 				        $response	= $data['Ds_Response'];
 				        $auth_code	= $data['Ds_AuthorisationCode'];
+
+				        // Reverse order code customization
+				        $order_id = apply_filters("wc_myredsys_merchant_order_decode", $data['Ds_Order']);
+				        // If not any change made, do default action
+				        if ( $order_id == $data['Ds_Order'] ) {
+				        	$order_id = substr( $data['Ds_Order'], 0, 8 );
+				        }
 				        
 				        // check to see if the response is valid
 				        if ( $received_signature === $calculated_signature
